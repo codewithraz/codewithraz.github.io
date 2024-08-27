b7ff3c012169db5d2c09944115ec50ead70380af
Razana <147581841+codewithraz@users.noreply.github.com>
2024-08-28 01:22:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+// Slideshow functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// Menu bar toggle functionality
 let menubar = document.querySelector('#menu-bar');
 let mynav = document.querySelector('.nav');
 let searchbar = document.querySelector('#search-bar');
 let search = document.querySelector('.search-form');
 
-
-
-menubar.onclick = () =>{
-    menubar.classList.toggle('fa-times')
-    mynav.classList.toggle('active')
+menubar.onclick = () => {
+    menubar.classList.toggle('fa-times');
+    mynav.classList.toggle('active');
 }
-searchbar.onclick = () =>{
+
+searchbar.onclick = () => {
     search.classList.toggle('active');
 }
-
